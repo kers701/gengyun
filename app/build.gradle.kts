e3c4d33398759cf3ca8acef4235699e292ce32db
kers701 <1481289@qq.com>
2026-09-13 04:29:39 +0800

    发布1.0.1配置签名

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.kers.gengyun"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 1001
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
